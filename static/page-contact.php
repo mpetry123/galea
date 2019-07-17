d9040dc1626d259ae2d0d77d6c5f34d604737696
Michael Petry <mpetry@fiberdesign.ca>
2019-07-17 15:31:57 -0400

    updates

--- static/page-contact.php
+++ static/page-contact.php
@@ -1,9 +1,58 @@
 <?php
 require_once('./header.php');
 ?>
-
-Contact
+<section id="hero" class="hero bg-black">
+    <div class="container">
+        <div class="multi-column">
+            <div class="callout">
+                <strong>GET IN <br> TOUCH</strong>
+                <div class="clip clip-m-shape with-content bg-white">
+                    <p>test 123 test 123 test 123</p>
+                    <p>test 123 test 123 test 123</p>
+                    <p>test 123 test 123 test 123</p>
+                </div>
+            </div>
+            <div class="media">
+                <img src="./dist/img/hero-feat-img-map.png" alt="About Us Featured Image" />
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="multi-column">
+            <div class="content">
+                <img src="./dist/img/feat-img-contact.png" alt="" />
+            </div>
+            <div class="content">
+                <h2 class="heading">Leave us a message</h2>
+                <form>
+                    <div>
+                        <label>Name</label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div>
+                        <label>Phone</label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div>
+                        <label>Message</label>
+                        <textarea name="message">
+                        </textarea>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 require_once('./footer.php');
 ?>
+
+
+
--- static/page-services.php
+++ static/page-services.php
@@ -13,106 +13,121 @@ require_once('./header.php');
                 </strong>
             </div>
             <div class="media">
-                <img src="./dist/img/hero-img-about.png" alt="About Us Featured Image" />
+                <img src="./dist/img/hero-feat-img-services.png" alt="" />
             </div>
         </div>
     </div>
 </section>
 <section id="about-us">
     <div class="container">
-        <div class="multi-column order-invert-desktop"">
+        <div class="multi-column">
             <div class="content">
-                <h2 class="heading">PLATELET RICH PLASMA (PRP) THERAPY</h2>
-                <p>Platelet Rich Plasma, therapy is a treatment designed to 
-                    <br>aid in the healing and regeneration of soft tissues such as 
-                    <br>tendons, ligaments and muscles. PRP is the injection of 
-                    <br>the patients own platelets, in high concentration into an 
-                    <br>injured tendon, ligament or muscle tear. The platelets 
-                    <br>contain growth factors which help stimulate natural healing.</p>
+                <div class="copy">
+                    <h2 class="heading">PLATELET RICH PLASMA (PRP)<br> THERAPY</h2>
+                    <p>Platelet Rich Plasma, therapy is a treatment designed to 
+                        <br>aid in the healing and regeneration of soft tissues such as 
+                        <br>tendons, ligaments and muscles. PRP is the injection of 
+                        <br>the patients own platelets, in high concentration into an 
+                        <br>injured tendon, ligament or muscle tear. The platelets 
+                        <br>contain growth factors which help stimulate natural healing.</p>
+                </div>
             </div>
             <div class="content">
-                <h2 class="heading">CYTORICH – PRP</h2>
-                <p>CytoRich-PRP is a new therapeutic product and is similar 
-                    <br>to PRP, but with an enriched anti-inflammatory/catabolic 
-                    <br>component. CytoRich-PRP comprises an abundant 
-                    <br>concentration of anti-inflammatory and regenerative 
-                    <br>agents that are intended to treat damaged and/or injured 
-                    <br>connective tissue, and chronic conditions such as 
-                    <br>tendonitis, muscle tears, and degenerative joint 
-                    <br>conditions such as osteoarthritis.</p>
-                <p>Unlike PRP, CytoRich-PRP has two components: a 
-                    <br>regenerative and an anti-inflammatory component. The 
-                    <br>regenerative component is similar to PRP, while the anti-
-                    <br>inflammatory component is enriched with anti-inflammatory, anti-
-                    <br>catabolic and regenerative proteins 
-                    <br>that increase CytoRich-PRP therapeutic potential 
-                    <br>comparing to PRP treatment.</p>
-                <p>CytoRich is an efficient and safe product for Osterarthritis 
-                    <br>treatment since it has been reported as a source of 
-                    <br>human bioactive molecules playing a key role in the 
-                    <br>fundamental processes of stimulating tissue reapir and 
-                    <br>regenerations. Health Canada Approval has been received 
-                    <br>for Indications for the treatment of Osteoarthritic Knee 
-                    <br>and Osteopathic Hip.</p>
+                <div class="copy">
+                    <h2 class="heading">CYTORICH – PRP</h2>
+                    <p>CytoRich-PRP is a new therapeutic product and is similar 
+                        <br>to PRP, but with an enriched anti-inflammatory/catabolic 
+                        <br>component. CytoRich-PRP comprises an abundant 
+                        <br>concentration of anti-inflammatory and regenerative 
+                        <br>agents that are intended to treat damaged and/or injured 
+                        <br>connective tissue, and chronic conditions such as 
+                        <br>tendonitis, muscle tears, and degenerative joint 
+                        <br>conditions such as osteoarthritis.</p>
+                    <p>Unlike PRP, CytoRich-PRP has two components: a 
+                        <br>regenerative and an anti-inflammatory component. The 
+                        <br>regenerative component is similar to PRP, while the anti-
+                        <br>inflammatory component is enriched with anti-inflammatory, anti-
+                        <br>catabolic and regenerative proteins 
+                        <br>that increase CytoRich-PRP therapeutic potential 
+                        <br>comparing to PRP treatment.</p>
+                    <p>CytoRich is an efficient and safe product for Osterarthritis 
+                        <br>treatment since it has been reported as a source of 
+                        <br>human bioactive molecules playing a key role in the 
+                        <br>fundamental processes of stimulating tissue reapir and 
+                        <br>regenerations. Health Canada Approval has been received 
+                        <br>for Indications for the treatment of Osteoarthritic Knee 
+                        <br>and Osteopathic Hip.</p>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<section>
-    <div class="container">
-        <div class="multi-column">
-            <div class="media">
-            <img src="./dist/img/parallax-test.png" id="parallax" style="max-height: 35rem;" alt="About Us Featured Image" />
+        <div class="multi-column align-vertical-center">
+            <div class="comtemt">
+                <img src="./dist/img/feat-img-procedure-package.png" alt="About Us Featured Image" />
             </div>
             <div class="content">
-                <h2 class="heading heading-section">Procedure package</h2>
+                <h2 class="heading stand-out">
+                    Procedure 
+                    <span class="large">package</span>
+                </h2>
             </div>
         </div>
         <div class="multi-column">
-            <div class="content-text">
+            <div class="content">
+                <div class="copy">
                 <h2 class="heading">FAT GRAFT</h2>
-                <p>Removal of patients’ fat (microlipoinjection) whereby the 
-                <br>physician will reinject the fat into the patient’s muscular 
-                <br>tear. This fat acts as a scaffold where the PRP injection 
-                <br>takes place and is held in place for healing.</p>
+                    <p>Removal of patients’ fat (microlipoinjection) whereby the 
+                    <br>physician will reinject the fat into the patient’s muscular 
+                    <br>tear. This fat acts as a scaffold where the PRP injection 
+                    <br>takes place and is held in place for healing.</p>
+                </div>
             </div>
-            <div class="content-text">
-                <h2 class="heading">ULTRASOUND</h2>
-                <p>Used in assisting physicians with precision injections.</p>
-            </div>
-        </div>
-        <div class="multi-column">
-            <div class="content-text">
-                <h2 class="heading">PLATELET RICH PLASMA (PRP) THERAPY</h2>
-                <p>Platelet Rich Plasma, used in healing muscle, ligament 
-                    <br>and tendon tears. Small and Large kits are used 
-                    <br>depending on the size and severity of the injury/tear.</p>
-            </div>
-            <div class="content-text">
-                <h2 class="heading">VISCO SUPPLEMENTATION</h2>
-                <p>Medical procedure during which lubricating fluid is 
-                    <br>injected into a joint to increase pain free mobility and 
-                    <br>fluidity of the joint.</p>
+            <div class="content">
+                <div class="copy">
+                    <h2 class="heading">ULTRASOUND</h2>
+                    <p>Used in assisting physicians with precision injections.</p>
+                </div>
             </div>
         </div>
         <div class="multi-column">
-            <div class="content-text">
-                <h2 class="heading">CYTORICH</h2>
-                <p>Stage 2 PRP used for the treatment of Osteoarthritic Knee 
+            <div class="content">
+                <div class="copy">
+                    <h2 class="heading">PLATELET RICH PLASMA (PRP) THERAPY</h2>
+                    <p>Platelet Rich Plasma, used in healing muscle, ligament 
+                        <br>and tendon tears. Small and Large kits are used 
+                        <br>depending on the size and severity of the injury/tear.</p>
+                    </div>
+            </div>
+            <div class="content">
+                <div class="copy">
+                    <h2 class="heading">VISCO SUPPLEMENTATION</h2>
+                    <p>Medical procedure during which lubricating fluid is 
+                        <br>injected into a joint to increase pain free mobility and 
+                        <br>fluidity of the joint.</p>
+                </div>
+            </div>
+        </div>
+        <div class="multi-column">
+            <div class="content">
+                <div class="copy">
+                    <h2 class="heading">CYTORICH</h2>
+                    <p>Stage 2 PRP used for the treatment of Osteoarthritic Knee 
                     <br>and Osteopathic hips.</p>
+                </div>
             </div>
-            <div class="content-text">
-                <h2 class="heading">SHOCKWAVE</h2>
-                <p>Shockwave is an acoustic wave which carries high energy 
-                    to painful spots of myoskeletal tissues with subacute, 
-                    sub-chronic and chronic conditions. The energy promotes 
-                    regeneration and reparative processes of the bones, 
-                    tendons, and other soft tissues. Shockwaves are 
-                    characterized by jump change in pressure, high amplitude 
-                    and non-periodicity. The kinetic energy of the projectile, 
-                    created by compressed air, is transferred to the 
-                    transmitter at the end of the applicator and further into 
-                    the tissue.</p>
+            <div class="content">
+                <div class="copy">
+                    <h2 class="heading">SHOCKWAVE</h2>
+                    <p>Shockwave is an acoustic wave which carries high energy 
+                    <br>to painful spots of myoskeletal tissues with subacute, 
+                    <br>sub-chronic and chronic conditions. The energy promotes 
+                    <br>regeneration and reparative processes of the bones, 
+                    <br>tendons, and other soft tissues. Shockwaves are 
+                    <br>characterized by jump change in pressure, high amplitude 
+                    <br>and non-periodicity. The kinetic energy of the projectile, 
+                    <br>created by compressed air, is transferred to the 
+                    <br>transmitter at the end of the applicator and further into 
+                    <br>the tissue.</p>
+                </div>
             </div>
         </div>
     </div>
